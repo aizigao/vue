bf5c958d403d87ab5d9c5566d6f9dce0656f3129
aizigao <weixiangzhe@live.com>
2025-11-29 19:15:08 +0800

    feat: update outlint

--- languages/vue/outline.scm
+++ languages/vue/outline.scm
@@ -1,29 +1,21 @@
-
 ; Comments appear as annotations in the outline
 (comment) @annotation
 
 ; ========== template =========
-; (template_element
-;   (start_tag
-;     (tag_name) @name
-;     (attribute (attribute_name) @attr_name (quoted_attribute_value (attribute_value) @attr_value))*
-;   )
-; ) @item
-
 (template_element
   (start_tag) @name
-  (attribute (attribute_name) @attr_name (quoted_attribute_value (attribute_value) @attr_value))*
 ) @item
 
 
 ; Custom elements/components (PascalCase or hyphenated) - self-closing tags
 (element
-    (start_tag) @name
+  (start_tag
+    (tag_name) @name)
 ) @item
 
-
 (element
-    (self_closing_tag) @name
+  (self_closing_tag
+    (tag_name) @name)
 ) @item
 
 
@@ -31,16 +23,10 @@
 
 (script_element
     (start_tag) @name
-    (raw_text) @context @item
-)
-
-(script_element
-    (end_tag) @name @item
-)
+) @item
 
 
 ; ========= style =========
 (style_element
     (start_tag) @name
-    (raw_text) @context
 ) @item
