e00a62c1c7aeba9ea62e0bbe9946fc7bda8ba73f
aizigao <weixiangzhe@live.com>
2025-12-01 00:29:05 +0800

    feat: try with className

--- languages/vue/outline.scm
+++ languages/vue/outline.scm
@@ -10,13 +10,51 @@
 ; Custom elements/components (PascalCase or hyphenated) - self-closing tags
 (element
   (start_tag
-    (tag_name) @name)
+    (tag_name) @name
+
+    (attribute
+      (attribute_name) @classAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @classAttr "class")
+    )?
+
+    (directive_attribute
+      (directive_argument) @argAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @argAttr "class")
+    )?
+
+    (attribute
+      (attribute_name) @classAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @classAttr "class")
+    )?
+  )
 ) @item
 
 (element
   (self_closing_tag
-    (tag_name) @name)
+    (tag_name) @name
+    (attribute
+      (attribute_name) @classAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @classAttr "class")
+    )?
+
+    (directive_attribute
+      (directive_argument) @argAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @argAttr "class")
+    )?
+
+    (attribute
+      (attribute_name) @classAttr
+      (quoted_attribute_value (attribute_value) @context)
+      (#eq? @classAttr "class")
+    )?
+  )
 ) @item
+
 
 
 ; ======= script ======
